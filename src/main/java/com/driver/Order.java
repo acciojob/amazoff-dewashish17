834257ae304557bce3cdd7ea8699f30d69ab0eb0
Dewashish Painkra <dewashishpainkra126@gmail.com>
2023-02-11 18:17:22 +0530

    Submission 1

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,11 +9,20 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        int hour=Integer.valueOf(deliveryTime.substring(0,2));
+        int minute=Integer.valueOf(deliveryTime.substring(3,5));
+        this.deliveryTime=hour*60+minute;
     }
 
     public String getId() {
         return id;
     }
+    public void setId(String id) {
+        this.id=id;
+    }
 
     public int getDeliveryTime() {return deliveryTime;}
+    public void setDeliveryTime() {
+        this.deliveryTime=deliveryTime;
+    }
 }
