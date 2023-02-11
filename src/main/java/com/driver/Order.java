b9af0cad3c712e794545389628264788309c1f3e
Dewashish Painkra <dewashishpainkra126@gmail.com>
2023-02-11 18:30:59 +0530

    submission 3

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -4,14 +4,17 @@ public class Order {
 
     private String id;
     private int deliveryTime;
+    public Order(){
 
+    }
     public Order(String id, String deliveryTime) {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
-        int hour=Integer.valueOf(deliveryTime.substring(0,2));
-        int minute=Integer.valueOf(deliveryTime.substring(3,5));
-        this.deliveryTime=hour*60+minute;
+        this.id = id;
+        Integer hour = Integer.valueOf(deliveryTime.substring(0, 2));
+        Integer minutes = Integer.valueOf(deliveryTime.substring(3));
+        this.deliveryTime = hour*60 + minutes;
     }
 
     public String getId() {
